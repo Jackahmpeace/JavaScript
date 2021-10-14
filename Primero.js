c666c05ba1c6889759122b9671e60d58f9272315
Jackahmpeace <yacampis@hotmail.com>
2021-10-14 20:44:48 -0300

    Primera version

---
+++ Primero.js
@@ -0,0 +1,6 @@
+var nombre = "Juan" /* nombre */
+var apellido = "Robles" /* apellido */
+var edad = 60 /* edad */
+console.log(`${nombre} ${apellido}, ${edad} años`) /* mostrar concatenado 1 */
+edad = edad + 10 /* suma un número */
+console.log(`${nombre} ${apellido}, ${edad} años`) /* mostrar concatenado 2 */
